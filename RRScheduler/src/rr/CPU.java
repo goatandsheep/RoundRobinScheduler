8d1918a2583b2d0c8cace14b073d2bf82534fd1e
Arangojd <arango.j.david@gmail.com>
2015-03-30 13:01:16 -0400

    Update CPU.java

--- RRScheduler/src/rr/CPU.java
+++ RRScheduler/src/rr/CPU.java
@@ -1,27 +1,32 @@
 package rr;
 
 public class CPU {
-	private static double quantum = 7.00; //quantum for the process
-	private static boolean locked = false;	
-	private static final Object lock = new Object();
+	private static double quantum = 7.00; // Quantum for the process
+	private static boolean locked = false;	// Lock flag
+	private static final Object lock = new Object(); //Lock object
 	
-	public static Process process(Process pro) throws InterruptedException {
+	
+	// This CPU method executes a given process.
+	// During execution the CPU is locked. Only one process can be executed at a time.
+	// Once done execution, GrimReaper unloads the process.
+	
+	public static void executeProcess(Process pro) throws InterruptedException {
 		synchronized(lock){
-		    if (locked)
-		    	lock.wait();
-		    else {
-		    	locked = true;
-		    	if (pro.getTime() >= quantum)
-		    		System.out.println("CPU: Process " +pro.getid() +" executed for " +quantum +" seconds.");
-		    	else
-		    		System.out.println("CPU: Process " +pro.getid() +" executed for " +pro.getTime() +" seconds.");
-		    	pro.execute(quantum);
-		    	GrimReaper.unloadProcess(pro);
-		    	locked = false;
-		    	lock.notify();
-		   }
-						
-		return pro;
+			
+			if (locked) // If locked wait to be notified.
+		    		lock.wait();
+		    	else // Else execute process 
+		    	{
+		    		locked = true; // Lock CPU
+		    		if (pro.getTime() >= quantum) // Output process execution details.
+		    			System.out.println("CPU: Process " +pro.getid() +" executed for " +quantum +" seconds.");
+		    		else
+		    			System.out.println("CPU: Process " +pro.getid() +" executed for " +pro.getTime() +" seconds.");
+		    		pro.execute(quantum); // Execute process.
+		    		GrimReaper.unloadProcess(pro); // Call GrimReaper to unload process.
+		    		locked = false; // Unlock CPU
+		    		lock.notify(); // Notify
+		   	}
 		}
 				
 	}	
