89f8a48a7e2df017e1147cb6932ce46aedc6bb4c
Arangojd <arango.j.david@gmail.com>
2015-03-30 15:09:06 -0400

    Update Generator.java

--- RRScheduler/src/rr/Generator.java
+++ RRScheduler/src/rr/Generator.java
@@ -4,14 +4,14 @@ public class Generator extends Thread {
 	
 	// This Generator thread creates a new process with an ID and a random execution time within a given range.
 	
-	static Thread run(double min, double max) throws InterruptedException {
+	public static Thread run(double min, double max) throws InterruptedException {
 		
 		int id = 1; // Process ID is a counter.
 		
 		while (true) // Run indefinitely. 
 		{
 			
-			if (ReadyQueue.getSize() < 20) // If the ready queue size is less then 20 create a new process. 
+			if (ReadyQueue.getSize() < 10) // If the ready queue size is less then 20 create a new process. 
 			{
 		    	    	
 				// The random execution time is generated within the range [min, max]. It is then rounded to two decimal places.
@@ -20,12 +20,12 @@ public class Generator extends Thread {
 				double execTime = Math.round((((Math.random() * (max - min)) + min))*100)/100;
 				
 				Process pro = new Process(id, execTime); // Creates new process with the given ID and the random execution time.  
-				ReadyQueue.addProcess(pro); // Loads new process into ready queue.
 				System.out.println("Generator: Process " +id +" loaded into ready queue."); // Outputs load details.
+				ReadyQueue.addProcess(pro); // Loads new process into ready queue.
 				id++; // Increment ID counter.
 			}
-			else // Else sleep for 1 second.
-	    		Thread.sleep(1000);
+			
+			Thread.sleep(50); 
 		}
 	}
 }
