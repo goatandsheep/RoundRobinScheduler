35cef0a42583e0eb568f9463754c7d6ff5a38494
Arangojd <arango.j.david@gmail.com>
2015-03-29 20:12:41 -0400

    Update Process.java

--- RRScheduler/src/rr/Process.java
+++ RRScheduler/src/rr/Process.java
@@ -1,37 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package rr;
 
-/**
- *
- * @author user
- */
 public class Process {
-    double id;
-    double execTime;
-    double tRemaining;
-    
-    /*Constructor
-     * from Generator, process could be created with random id and random execTime.
-     * since it's a new process time remaining is same as execution time.*/
-    public Process(double id, double execTime){
-    	this.id = id;
-    	this.execTime = execTime;
-    	this.tRemaining = execTime;}
-    
-    /* Method
-     * After one quantum, process updates it's time remaining as decreasing amount of quantum .
-     */
-    public void afterQuantum(double quantum){
-    	
-    	this.tRemaining = this.tRemaining - quantum;
-    	if (tRemaining <0) {
-    		this.tRemaining =0;    	}
-    	
-    }
+	
+	double id;
+	double execTime;
+	double tRemaining;
 
+	/*Constructor
+	 * from Generator, process could be created with random id and random execTime.
+	 * since it's a new process time remaining is same as execution time.*/
 
+	public Process(double id, double execTime) {
+		this.id = id;
+		this.execTime = execTime;
+		this.tRemaining = execTime;
+	}
+
+	/* Method
+	 * After one quantum, process updates it's time remaining as decreasing amount of quantum .
+	 */
+
+	public void afterQuantum(double quantum) {
+		if (tRemaining > 0) {
+			this.tRemaining = this.tRemaining - quantum; 
+		}
+		else
+			this.tRemaining = 0;
+	}
 }
