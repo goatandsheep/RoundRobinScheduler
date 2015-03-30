242679fd8814aefc3d0da255668b1cc629fad542
Arangojd <arango.j.david@gmail.com>
2015-03-30 14:18:37 -0400

    Update Dispatcher.java

--- RRScheduler/src/rr/Dispatcher.java
+++ RRScheduler/src/rr/Dispatcher.java
@@ -3,15 +3,19 @@ package rr;
 public class Dispatcher {
 	
 	
-	// This Dispatcher method loads the next process in the ready queue to the CPU.
+	// This Dispatcher thread loads the next process in the ready queue to the CPU.
 	
-	public void loadProcess() throws InterruptedException {
-		if (ReadyQueue.getSize() > 0) // If the queue is not empty a process is loaded into the CPU.
+	static Thread loadProcess() throws InterruptedException {
+		while(true) // Run indefinitely.
 		{
-			Process pro = ReadyQueue.removeProcess(); // Get process from the ready queue.
-			System.out.println("Dispatcher: Process " +pro.getid() +" loaded into CPU."); // Output CPU loading details.
-			CPU.executeProcess(pro); // Load process into CPU.
+			if (ReadyQueue.getSize() > 0) // If the queue is not empty a process is loaded into the CPU.
+			{
+				Process pro = ReadyQueue.removeProcess(); // Get process from the ready queue.
+				System.out.println("Dispatcher: Process " +pro.getid() +" loaded into CPU."); // Output CPU loading details.
+				CPU.executeProcess(pro); // Load process into CPU.
+			}
+			else // Else sleep for 1 second.
+				Thread.sleep(1000);
 		}
 	}
-
 }
