c455d7d266cc26790d69b1d09f4fa1f655341099
Arangojd <arango.j.david@gmail.com>
2015-03-30 13:08:48 -0400

    Update GrimReaper.java

--- RRScheduler/src/rr/GrimReaper.java
+++ RRScheduler/src/rr/GrimReaper.java
@@ -2,12 +2,15 @@ package rr;
 
 public class GrimReaper {
 	
+	
+	// This GrimReaper method unloads a given process from the CPU. 
+	// If the process has not finished executing, the process is added to the ready queue.
+	
 	public static void unloadProcess(Process pro){
-		if (pro.getTime() == 0) {
-			ReadyQueue.removeProcess();
+		if (pro.getTime() == 0) // If the process finished executing output details.
 			System.out.println("Reaper: Process " +pro.getid() +" finished executing.");
-		}
-		else {
+		else // Else add process to ready queue and output details.
+		{
 			ReadyQueue.addProcess(pro); 
 			System.out.println("Reaper: Process " +pro.getid() +" recycled into ready queue with " +pro.getTime() +" second(s) remaining.");
 		}
