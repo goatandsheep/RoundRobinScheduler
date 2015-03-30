aa7a4b82a7f98fd22d515df3781b7d1f6d8b522d
Arangojd <arango.j.david@gmail.com>
2015-03-30 14:17:43 -0400

    Update RRScheduler.java

--- RRScheduler/src/rr/RRScheduler.java
+++ RRScheduler/src/rr/RRScheduler.java
@@ -1,41 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package rr;
 
-/**
- *
- * @author user
- */
 public class RRScheduler {
-	
-	/*
-	 * from LTSA
-	 * 
-	 * || RR_SCHEDULER = (QUEUE || GENERATOR || DISPATCHER || CPU || GRIMREAPER).
-	 * 
-	 */
-    public void main(String[] args){
-    	int i = 1;
-    	while(true){
-        	if (ReadyQueue.getSize() < 20){
-        		
-        		Generator.run(i);
-        		//cap queue size at 20	
-    		}
-        	else{
-        		try {
-					wait();
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-        	}
-        	i++;
-    	}
+
+	public void main(String[] args) throws InterruptedException {
     	
-    	
+		double min = 1.00, max = 10.00;
+		
+		Generator.run(min, max);
+		Dispatcher.loadProcess();	
     }
 }
