ed7f882d26b060f3b7bbb7d22d0a74292ad33de3
Arangojd <arango.j.david@gmail.com>
2015-03-30 13:25:33 -0400

    Update ReadyQueue.java

--- RRScheduler/src/rr/ReadyQueue.java
+++ RRScheduler/src/rr/ReadyQueue.java
@@ -2,36 +2,33 @@ package rr;
 
 import java.util.*;
 
+
+// This ReadyQueue class models a ready queue object.
+
 public class ReadyQueue {
 	
+	// Instantiating LinkedList theQueue that will be used as a ready queue.
+	
 	private static Queue<Process> theQueue= new LinkedList<Process>();
-	//private LinkedList<Process> queue = new LinkedList<Process>();
-	//public int size;
-	/*
-	ReadyQueue() {
-		theQueue = new LinkedList();
-	}*/
+	
+	// This ReadyQueue method adds a given process the end of the ready queue.
 	
 	public static void addProcess(Process pro) {
-		theQueue.add(pro);
+		theQueue.add(pro); // Add process to the queue.
 	}
 	
+	// This ReadyQueue method removes the next process in the ready queue and returns a copy of the object.
+	
 	public static Process removeProcess() {
-		if (getSize() > 0) {
-			/*Process nextPro = this.queue.getFirst();
-			this.queue.removeFirst();
-			this.size--;
-			return nextPro;*/
-			
+		if (getSize() > 0) // If the queue is greater than 0, remove/return the next process.
 			return theQueue.remove();
-		}
-		else
+		else // Else return null.
 			return null;
-		
 	}
+	
+	// Get ready queue size value.
 	
 	public static int getSize() {
 		return theQueue.size();
 	}
-
 }
