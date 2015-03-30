22ac7591cd831121485a222054a1a9f1571d30a1
Arangojd <arango.j.david@gmail.com>
2015-03-30 14:18:12 -0400

    Update Generator.java

--- RRScheduler/src/rr/Generator.java
+++ RRScheduler/src/rr/Generator.java
@@ -1,30 +1,31 @@
 package rr;
 
-public class Generator {
+public class Generator extends Thread {
 	
+	// This Generator thread creates a new process with an ID and a random execution time within a given range.
 	
-
-
-	public static void run(int id){
-		//add process to end of queue
-		double execTime = (Math.random()*12); 
-		Process pro = new Process(id, execTime); //pass in arguements of time 
-		ReadyQueue.addProcess(pro);
-		System.out.println("Generator: Process " +id +" loaded into ready queue.");
-	}
-
-	// This Generator method creates a new process with a given process ID and with a random execution time within a given range.
-
-	
-	public void run(int id, double min, double max) {
+	static Thread run(double min, double max) throws InterruptedException {
 		
-		// The random execution time is generated within the range [min, max]. It is then rounded to two decimal places.
-		// The number is multiplied by 100, rounded to the nearest int, then multiplied by 100 to leave two decimals of accuracy.
+		int id = 1; // Process ID is a counter.
 		
-		double execTime = Math.round((((Math.random() * (max - min)) + min))*100)/100;
-		
-		Process pro = new Process(id, execTime); // Creates new process with the given ID and the random execution time.  
-		ReadyQueue.addProcess(pro); // Loads new process into ready queue.
-		System.out.println("Generator: Process " +id +" loaded into ready queue."); // Outputs load details.
+		while (true) // Run indefinitely. 
+		{
+			
+			if (ReadyQueue.getSize() < 20) // If the ready queue size is less then 20 create a new process. 
+			{
+		    	    	
+				// The random execution time is generated within the range [min, max]. It is then rounded to two decimal places.
+				// The number is multiplied by 100, rounded to the nearest int, then multiplied by 100 to leave two decimals of accuracy.
+				
+				double execTime = Math.round((((Math.random() * (max - min)) + min))*100)/100;
+				
+				Process pro = new Process(id, execTime); // Creates new process with the given ID and the random execution time.  
+				ReadyQueue.addProcess(pro); // Loads new process into ready queue.
+				System.out.println("Generator: Process " +id +" loaded into ready queue."); // Outputs load details.
+				id++; // Increment ID counter.
+			}
+			else // Else sleep for 1 second.
+	    		Thread.sleep(1000);
+		}
 	}
 }
