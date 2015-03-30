b76d90852206278c988006479266518d06d4cc0c
gyen92 <gyen92@gmail.com>
2015-03-30 09:08:21 +0900

    Update Process.java

--- RRScheduler/src/rr/Process.java
+++ RRScheduler/src/rr/Process.java
@@ -10,5 +10,28 @@ package rr;
  * @author user
  */
 public class Process {
+    double id;
+    double execTime;
+    double tRemaining;
     
+    /*Constructor
+     * from Generator, process could be created with random id and random execTime.
+     * since it's a new process time remaining is same as execution time.*/
+    public Process(double id, double execTime){
+    	this.id = id;
+    	this.execTime = execTime;
+    	this.tRemaining = execTime;}
+    
+    /* Method
+     * After one quantum, process updates it's time remaining as decreasing amount of quantum .
+     */
+    public void afterQuantum(double quantum){
+    	
+    	this.tRemaining = this.tRemaining - quantum;
+    	if (tRemaining <0) {
+    		this.tRemaining =0;    	}
+    	
+    }
+
+
 }
